b8f6b546c73cc2a9971f36ecd4f9c1e3b366eb5f
Raja-AR-007 <92211139+Raja-AR-007@users.noreply.github.com>
2022-01-31 16:53:24 +0530

    Add files via upload

---
+++ _44_Access_Modifiers/Access_Modifiers.java
@@ -0,0 +1,14 @@
+package com.Concepts.Access_Modifiers;
+import com.Concepts.Access_Modifiers.test.Student;
+
+public class Access_Modifiers {
+	public static void main (String args[]) {
+		
+		Student s=new Student();
+		s.rollNo=9;
+		s.name="Maha";
+		s.age=23;
+		s.marks=66;
+	}
+
+}
---
+++ _44_Access_Modifiers/Engineer.java
@@ -0,0 +1,14 @@
+package com.Concepts.Access_Modifiers;
+import com.Concepts.Access_Modifiers.test.Student;  //Student file is imported here
+
+public class Engineer extends Student {
+	
+	public void show() {
+		rollNo=2;
+		name="Jagan"
+		marks=77;
+		age=32;
+		
+	}
+	
+}
